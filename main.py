23b29ca25b998032e84f48f945cfec047b2b7bf5
Lucid Dreamer <shuklamanasofficial@gmail.com>
2021-05-31 07:24:20 +0530

    Comments and loop added

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 import pyttsx3
 import PyPDF2
+
 book = open('testpdf.pdf','rb')
+#Change the testbook.pdf with your own book make sure it is in the same directory as main.
+
 pdfreader = PyPDF2.PdfFileReader(book)
 pages = pdfreader.numPages
 print(pages)
 speaker = pyttsx3.init()
-page = pdfreader.getPage(7)
-text = page.extractText()
-speaker.say(text)
-speaker.runAndWait()
+
+#You can initialize the starting and ending page by changing 7 and pages respectively.
+
+for num in range(10,pages):
+    page = pdfreader.getPage(num)
+    text = page.extractText()
+    speaker.say(text)
+    speaker.runAndWait()
